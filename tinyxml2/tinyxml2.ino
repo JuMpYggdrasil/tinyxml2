739874c3498285247d0d9498261ea5d2072bf786
JuMpYggdrasil <70381321+JuMpYggdrasil@users.noreply.github.com>
2020-12-05 19:51:29 +0700

    Update tinyxml2.ino

--- tinyxml2/tinyxml2.ino
+++ tinyxml2/tinyxml2.ino
@@ -5,20 +5,20 @@ using namespace tinyxml2;
 XMLDocument xmlDocument;
 //char * testDocument = "<root><element>7</element></root>";
 //String testDocument = "<root><element>7</element></root>";
-String testDocument = "<confirmed-RequestPDU><element>7</element><invokeID>2676121</invokeID><ConfirmedServiceRequest><read specificationWithResult=\"false\"><variableAccessSpecification><listOfVariable><SEQUENCE><variableSpecification><name><domain-specific><domainId>IEDNameLDINst</domainId><itemId>LLN0$ST$Beh$stVal</itemId></domain-specific></name></variableSpecification></SEQUENCE></listOfVariable></variableAccessSpecification></read></ConfirmedServiceRequest></confirmed-RequestPDU>";
+String testDocument = "<confirmed-RequestPDU><!-- this is comment 1 --><element>7</element><invokeID>2676121</invokeID><!-- this is comment 2 --><ConfirmedServiceRequest><read specificationWithResult=\"false\"><variableAccessSpecification><listOfVariable><SEQUENCE><variableSpecification><name><domain-specific><domainId>IEDNameLDINst</domainId><itemId>LLN0$ST$Beh$stVal</itemId></domain-specific></name></variableSpecification></SEQUENCE></listOfVariable></variableAccessSpecification></read></ConfirmedServiceRequest></confirmed-RequestPDU>";
 
 int val;
 
 void setup() {
     Serial.begin(115200);
-
+    
     if (xmlDocument.Parse(testDocument.c_str()) != XML_SUCCESS) {
         Serial.println("Error parsing");
         return;
     };
 
-    XMLNode * root = xmlDocument.FirstChild();
-    XMLElement * element = root->FirstChildElement("element");
+    XMLNode* root = xmlDocument.FirstChild();
+    XMLElement* element = root->FirstChildElement("element");
 
     //int val;
     element->QueryIntText(&val);
@@ -27,8 +27,17 @@ void setup() {
 }
 
 void loop() {
-    XMLNode* root = xmlDocument.FirstChild();
-    XMLNode* domain_specific = root->FirstChildElement("ConfirmedServiceRequest")->FirstChildElement("read")->FirstChildElement("variableAccessSpecification")->FirstChildElement("listOfVariable")->FirstChildElement("SEQUENCE")->FirstChildElement("variableSpecification")->FirstChildElement("name")->FirstChildElement("domain-specific");
+    XMLElement* root = xmlDocument.RootElement();//confirmed-RequestPDU
+    XMLElement* readElement = root->FirstChildElement("ConfirmedServiceRequest")->FirstChildElement("read");
+    XMLElement* domain_specific = readElement->FirstChildElement("variableAccessSpecification")->FirstChildElement("listOfVariable")->FirstChildElement("SEQUENCE")->FirstChildElement("variableSpecification")->FirstChildElement("name")->FirstChildElement("domain-specific");
+
+    boolean attribute;
+    if (readElement->QueryBoolAttribute("specificationWithResult", &attribute) != XML_SUCCESS) {
+        Serial.println("Could not obtain the attribute");
+        return;
+    };
+    Serial.println(attribute);
+
 
     XMLElement* invokeIdElement = root->FirstChildElement("invokeID");
     const char* invokeID = invokeIdElement->GetText();
